cedab1e52d3a8156fc614601dce883cfbf7d14cf
Aaron Brethorst <aaron@brethorsting.com>
2018-01-13 17:01:39 -0800

    Gets the example app working

--- Example 004/ViewController.m
+++ Example 004/ViewController.m
@@ -7,23 +7,95 @@
 //
 
 #import "ViewController.h"
+@import Firebase;
+@import SafariServices;
 
-@interface ViewController ()
+static NSString * const kTableViewCellReuseIdentifier = @"identifier";
+
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property(nonatomic,strong) UITableView *tableView;
+@property(nonatomic,strong) NSArray<NSDictionary*> *tableData;
+@property(nonatomic,strong) FIRDatabaseReference *dbReference;
 
 @end
 
 @implementation ViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
+    if (self) {
+        self.title = NSLocalizedString(@"Firebase Table Example",);
+        _tableData = @[];
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.dbReference = [[FIRDatabase database] reference];
+
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+
+    [self loadData];
 }
 
+#pragma mark - Firebase
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)loadData {
+    FIRDatabaseQuery *query = [[self.dbReference child:@"users"] queryOrderedByChild:@"username"];
+
+    [query observeSingleEventOfType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot *snapshot) {
+        // For the database structure that I have defined (see the data.json file in this project),
+        // I will expect to see an array of dictionaries in snapshot.value.
+        self.tableData = snapshot.value;
+        self.title = [NSString stringWithFormat:NSLocalizedString(@"%@ Users",), @(self.tableData.count)];
+        [self.tableView reloadData];
+    } withCancelBlock:^(NSError *error) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Error",) message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Dismiss",) style:UIAlertActionStyleCancel handler:nil]];
+        [self presentViewController:alert animated:YES completion:nil];
+    }];
 }
 
+#pragma mark - Table View
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.tableData.count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kTableViewCellReuseIdentifier];
+
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:kTableViewCellReuseIdentifier];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+
+    // For the database structure that I have defined (see the data.json file in this project),
+    // I will expect that the user dictionary below has three keys: name, username, and website.
+
+    NSDictionary *user = self.tableData[indexPath.row];
+
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ (%@)", user[@"name"], user[@"username"]];
+    cell.detailTextLabel.text = user[@"website"];
+
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *user = self.tableData[indexPath.row];
+    NSURL *URL = [NSURL URLWithString:user[@"website"]];
+
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+    SFSafariViewController *safariController = [[SFSafariViewController alloc] initWithURL:URL];
+    [self presentViewController:safariController animated:YES completion:nil];
+}
 
 @end
---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Example 004' do
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+end
